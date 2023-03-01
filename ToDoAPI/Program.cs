286bfc584840365c160fa028a25232af0372722d
Jesse Galloway <JessGalloway23@gmail.com>
2023-02-28 22:47:44 -0600

    Scaffolded DB context, Added Cors functionality

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,11 +1,42 @@
+//Step 2 Scaffold-DbContext in package manager console
+
+
+//Step 3 add the using statments
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Models;
+
+// Questions for the instructor:
+    //When does ToDo become API? the only thing named ToDoAPI is the project name everything else is ToDo.
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Adding Cors
+builder.Services.AddCors(options => 
+{
+    options.AddDefaultPolicy(policy => 
+    {
+        policy.WithOrigins("OriginsPolicy", "http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+    });
+
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Step 4 addd the Db context service to initiate the Db connection 
+builder.Services.AddDbContext<ToDoAPI.Models.ToDoContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoDB"));
+
+});
+
+
+
 
 var app = builder.Build();
 
@@ -21,5 +52,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors();
 
 app.Run();
